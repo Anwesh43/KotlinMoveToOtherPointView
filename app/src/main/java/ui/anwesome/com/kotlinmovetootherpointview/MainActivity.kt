c29147ffed383350de97530b0e8223cf0828b84e
Anwesh43 <anweshthecool0@gmail.com>
2018-01-08 23:52:05 +0530

    using MovetoOtherPointView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinmovetootherpointview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinmovetootherpointview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlinmovetootherpointview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.movetootherpointview.MoveToOtherPointView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MoveToOtherPointView.create(this)
     }
 }
